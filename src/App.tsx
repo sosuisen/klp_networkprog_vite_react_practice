3aaab20c35741509454abe7ad5e3eb2b51fd22f1
sosuisen <hidekazu.kubota@gmail.com>
2024-09-11 22:47:08 +0900

    fix: add biome

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,34 @@
-import './App.css'
-
 type Todo = {
-  id: number;
-  title: string;
-  completed: boolean;
+	id: number;
+	title: string;
+	completed: boolean;
 };
 
-function App() {
-  const todos: Todo[] = [
-    { id: 1, title: "ネーム", completed: true },
-    { id: 2, title: "下書き", completed: false },
-  ];
+const todos: Todo[] = [
+	{ id: 1, title: "ネーム", completed: true },
+	{ id: 2, title: "下書き", completed: false },
+];
 
-  return (
-    <div>
-      <h1>ToDoリスト</h1>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>
-            <span style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}>
-              {todo.title}
-            </span>
-            <span> - {todo.completed ? "完了" : "未完了"}</span>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-}
+const App = () => {
+	return (
+		<div>
+			<h1>ToDoリスト</h1>
+			<ul>
+				{todos.map((todo) => (
+					<li key={todo.id}>
+						<span
+							style={{
+								textDecoration: todo.completed ? "line-through" : "none",
+							}}
+						>
+							{todo.title}
+						</span>
+						<span> - {todo.completed ? "完了" : "未完了"}</span>
+					</li>
+				))}
+			</ul>
+		</div>
+	);
+};
 
-export default App
+export default App;
