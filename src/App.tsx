195d3f7b862f359fe4bda0544ff921fe55051b8d
Hidekazu Kubota <hidekazu.kubota@gmail.com>
2024-09-11 23:05:33 +0900

    fix: use biome

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,22 @@
-import './App.css';
-import { Todo } from './types';
-import TodoItem from './TodoItem';
+import type { Todo } from "./types";
+import TodoItem from "./TodoItem";
 
-function App() {
-  const todos: Todo[] = [
-    { id: 1, title: "ネーム", completed: true },
-    { id: 2, title: "下書き", completed: false },
-  ];
+const todos: Todo[] = [
+	{ id: 1, title: "ネーム", completed: true },
+	{ id: 2, title: "下書き", completed: false },
+];
 
-  return (
-    <div>
-      <h1>ToDoリスト</h1>
-      <ul>
-        {todos.map((todo) => (
-          <TodoItem key={todo.id} todo={todo} />
-        ))}
-      </ul>
-    </div>
-  );
-}
+const App = () => {
+	return (
+		<div>
+			<h1>ToDoリスト</h1>
+			<ul>
+				{todos.map((todo) => (
+					<TodoItem key={todo.id} todo={todo} />
+				))}
+			</ul>
+		</div>
+	);
+};
 
-export default App
+export default App;
--- src/TodoItem.tsx
+++ src/TodoItem.tsx
@@ -1,21 +1,23 @@
-import React from 'react';
-import { Todo } from './types';
+import type React from "react";
+import type { Todo } from "./types";
 
 // TodoItem コンポーネントのプロパティ型定義
 type TodoItemProps = {
-  todo: Todo;
+	todo: Todo;
 };
 
 // Todo項目を表示するコンポーネント
 const TodoItem: React.FC<TodoItemProps> = ({ todo }) => {
-  return (
-    <li>
-      <span style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}>
-        {todo.title}
-      </span>
-      <span> - {todo.completed ? "完了" : "未完了"}</span>
-    </li>
-  );
+	return (
+		<li>
+			<span
+				style={{ textDecoration: todo.completed ? "line-through" : "none" }}
+			>
+				{todo.title}
+			</span>
+			<span> - {todo.completed ? "完了" : "未完了"}</span>
+		</li>
+	);
 };
 
 export default TodoItem;
