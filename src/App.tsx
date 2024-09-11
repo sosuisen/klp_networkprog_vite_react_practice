ba593dd70fce6131c0ad6ce2bca69c5bb6c8dd79
Hidekazu Kubota <hidekazu.kubota@gmail.com>
2024-09-11 23:38:28 +0900

    fix: add useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,43 @@
+import { useState } from "react";
+
 type Todo = {
 	id: number;
 	title: string;
 	completed: boolean;
 };
 
-const todos: Todo[] = [
+const initialTodos: Todo[] = [
 	{ id: 1, title: "ネーム", completed: true },
 	{ id: 2, title: "下書き", completed: false },
+	{ id: 3, title: "ペン入れ", completed: false },
+	{ id: 4, title: "トーン貼り", completed: false },
+	{ id: 5, title: "仕上げ", completed: false },
 ];
 
 const App = () => {
+	// useStateでtodosの状態を管理
+	const [todos, setTodos] = useState<Todo[]>(initialTodos);
+
+	// タスクの完了/未完了をトグルする関数
+	const toggleTodo = (id: number) => {
+		setTodos((prevTodos) =>
+			prevTodos.map((todo) =>
+				todo.id === id ? { ...todo, completed: !todo.completed } : todo,
+			),
+		);
+	};
+
 	return (
 		<div>
 			<h1>ToDoリスト</h1>
 			<ul>
 				{todos.map((todo) => (
-					<li key={todo.id}>
+					// biome-ignore lint/a11y/useKeyWithClickEvents: <explanation>
+					<li
+						key={todo.id}
+						onClick={() => toggleTodo(todo.id)}
+						style={{ cursor: "pointer" }}
+					>
 						<span
 							style={{
 								textDecoration: todo.completed ? "line-through" : "none",
